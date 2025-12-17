42db07ac60272bf48af55605f6e1a5c749a81591
Valerii Rudyka <val.rudika@gmail.com>
2025-12-17 23:53:26 +0200

    changed

--- delivery-website/Resources/SharedResources.Designer.cs
+++ delivery-website/Resources/SharedResources.Designer.cs
@@ -22,16 +22,16 @@ namespace delivery_website.Resources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public partial class SharedResources {
+    public class SharedResources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        public SharedResources() {
+        internal SharedResources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
